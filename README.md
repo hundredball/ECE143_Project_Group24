c5408fe08bc1150720b7589775b9ec03c0f55297
徐玠 <hundredball@xujiede-mbp.attlocal.net>
2020-02-12 21:51:43 -0800

    Update dataloader.py, move data to google drive and create some plot functions

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ECE143_Project
 ## dataloader.py
-It is used to get a dataframe of all the records in given years.
+load(years) is used to get a dataframe of all the records in given years.  
+clean_concat_data() is used to concatenate years of data and unify type of address_zip, and finally exports csv file.  
+Data: 
+https://drive.google.com/file/d/1jppOS7tBgxhGE_7A9b9rmbnLK9rQbLWv/view?usp=sharing
+## plot_time.py
+plot_incident_time(data, unit_time) is used to plot counts of incidents over months or years.
+
--- dataloader.py
+++ dataloader.py
@@ -8,26 +8,45 @@ Created on Thu Feb  6 10:20:43 2020
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-def load(years = list(range(2007,2021))):
+def load(years = range(2007,2021)):
     '''
     
 
     Parameters
     ----------
-    years : list
-        Fire incidents of these years will be imported. The default is list(range(2007,2021)).
+    years : iterable
+        Fire incidents of these years will be imported. The default is range(2007,2021).
 
     Returns
     -------
     data : pandas.dataframe
-        Fire incident of yeas
+        Fire incident of years
 
     '''
     
-    assert isinstance(years, list)
+    assert hasattr(years, '__iter__')
     assert all(2007<=i<=2020 and isinstance(i, int) for i in years)
     
+    data = []
+    path = './data/all_fd_incidents.csv'
+    try:
+        data = pd.read_csv(path)
+    except OSError as e:
+        print(e)
+        return None
+    data = data[[(year_one_data in years) for year_one_data in data['year_response']]]
+    
+    return data
+
+def clean_concat_data():
+    '''
+    Concat data from 2003 to 2020, and unify type of address_zip 
+    Take over an hour
+
+    '''
+    years = list(range(2007,2021))
     data = []
     path = './data/fd_incidents_year_datasd_v1.csv'
     for year in years:
@@ -35,31 +54,48 @@ def load(years = list(range(2007,2021))):
         data.append(data_year)
         
     data = pd.concat(data)
+    data.reset_index(drop=True, inplace=True)
     
-    return data
-
-data = load()
-years = list(range(2007,2021))
-
-# Counts of incidents over years
-events_count = [len(data[data['year_response']==year]) for year in years]
-dict_count = dict(zip(years,events_count))
-
-plt.figure()
-plt.plot(years, events_count)
-plt.title('Counts of incidents over years')
-
-plt.show()
-
-# Create a table for counts of each incident over years
-call_categories = data['call_category'].unique()
-problem_categories = data['problem'].unique()
-dict_event = dict()
-
-for year in years:
-    event_count_year = [len(data[(data['call_category'] == event) & (data['year_response'] == year)]) for event in call_categories]
-    dict_event[year] = event_count_year
+    # eliminate incidents lacking information (1749273 -> 1724049)
+    data.dropna(inplace=True)
     
-df_event_counts = pd.DataFrame.from_dict(dict_event)
-dict_categories = dict( zip(range(len(call_categories)), call_categories) )
-df_event_counts.rename(index=dict_categories, inplace = True)
+    # load zipcodes
+    f = open('./data/SanDiego_zipcode.txt', 'r')
+    str_zip_codes = f.readline()
+    f.close()
+    str_zip_codes = str_zip_codes.split(', ')
+    zip_codes = set([int(zip_code) for zip_code in str_zip_codes])    
+    
+    # unify the type of address zip () (1724049 -> 1724027)
+    # check if it's correct zip code in San Diego (1724027 -> 1713001)
+    i = 0
+    for index, row in data.iterrows():
+        try:
+            i += 1
+            print(i)
+            int_zip = int(row['address_zip'])
+            if int_zip in zip_codes:
+                data.at[index, 'address_zip'] = int_zip
+            else:
+                data.drop(index, inplace=True)
+        except:
+            data.drop(index, inplace=True)
+            
+    data.to_csv(r'./data/all_fd_incidents.csv')
+    
+
+if __name__ == '__main__':
+    
+    # check if types of properties of every years are unified
+    data = load()
+    years = list(range(2007, 2021))
+    type_properties = []
+    
+    for prop in data.columns:
+        elements = data[prop].unique()
+        same_type_unique_prop = all(type(unique_prop)==type(elements[0]) \
+                                     for unique_prop in elements)
+        type_properties.append(same_type_unique_prop)
+
+
+
