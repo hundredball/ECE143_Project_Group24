eba07442bea401ee9be24c34976cec73e619b9ef
Sidharth2905 <sidharth.suresh09@gmail.com>
2020-03-12 16:01:13 -0700

    Add files via upload

---
+++ trends.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import os
+import glob
+import seaborn as sns
+import functools
+from datetime import datetime
+from sorted_months_weekdays import *
+
+from sort_dataframeby_monthorweek import *
+
+
+# In[ ]:
+
+
+def yearly_trends(df, pop_df):
+    '''Plot the monthly and yearly trends of the incidents
+    input: df - Pandas Dataframe'''
+    assert isinstance(df, pd.DataFrame)
+    assert isinstance(pop_df, pd.DataFrame)
+    df.drop(df.loc[df['year_response']== 2020].index, inplace=True)   ## Remove year 2020 data
+    
+    ##yearly trends
+    year_count  = df['year_response'].value_counts()
+    plt.figure(figsize=(10,5))
+    ax = sns.lineplot(x = year_count.index, y = year_count.values, color = 'r', marker = 'o')
+    ax.yaxis.set_major_formatter(
+        matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+    plt.title('Year-to-Year Trend')
+    ax.set_ylabel('Number of incidents', fontsize=14, color = 'red')
+    ax2 = ax.twinx()
+    ax2 = sns.lineplot(x = pop_df['Year'], y = pop_df['Population'], marker = 'o')
+    ax2.yaxis.set_major_formatter(
+        matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+    plt.title('Year-to-Year Trend')
+    ax.set_ylabel('Number of incidents', fontsize=14, color = 'red')
+    ax.set_xlabel('Year', fontsize=12)
+    ax2.set_ylabel('Population', fontsize = 14, color = 'blue')
+    plt.savefig('yearlyincidents.jpg')
+    plt.show()
+    
+
+
+# In[7]:
+
+
+def monthly_trends(df):
+## month to month trends
+assert isinstance(df, pd.DataFrame)
+df.drop(df.loc[df['year_response']== 2020].index, inplace=True)   ## Remove year 2020 data
+month_count  = df['month_response'].value_counts()
+fig, ax = plt.subplots(figsize=(10,5))
+ax = sns.lineplot(month_count.index, month_count.values, color = 'orangered', marker = 'o')
+ax.yaxis.set_major_formatter(
+    matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+plt.title('Month-to-Month Trend')
+plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12], ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct',' Nov', 'Dec'])
+plt.ylabel('Number of incidents', fontsize=12)
+plt.xlabel('Month', fontsize=12)
+plt.savefig('month2month.jpg')
+plt.show()
+
